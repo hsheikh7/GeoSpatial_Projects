dc1bed411f5b3d64e7ac5120ea2d3cc9ce7bc911
hsheikh7 <hassan.sheikh85@gmail.com>
2023-09-09 22:53:49 +0330

    Created the main project and geospatial app

--- Geospatial_Django/Scripts/activate.bat
+++ Geospatial_Django/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Hassan\Documents\Projects_Python\MyWebsite\Geospatial_Django
+set VIRTUAL_ENV=C:\Users\Hassan\Documents\Projects_Python\MyWebsite\GeoSpatial_Projects\Geospatial_Django
 
 if not defined PROMPT set PROMPT=$P$G
 
