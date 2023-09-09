7f2b61a39bc1e9dd2f7357cd1797d7097311c7ee
hsheikh7 <hassan.sheikh85@gmail.com>
2023-09-09 23:41:44 +0330

    added map functions

--- Geospatial_Django/geospatial/urls.py
+++ Geospatial_Django/geospatial/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from geospatial.views import *
 from django.contrib.auth import views as auth_views
-from . import views
+# from . import views
 
 urlpatterns = [
-   path('',views.home, name='home')
+   path('',home, name='home')
+   
 ]
